ec618616869552f3f0175504cfe0b9839deb0c2e
keyna <keynacastrofuentes@gmail.com>
2025-08-24 22:08:50 -0600

    nuevos cambios

--- js/admin-global.js
+++ js/admin-global.js
@@ -29,6 +29,40 @@ function hideAddLocalForm() {
     document.getElementById('add-local-form').style.display = 'none';
 }
 
+{/* <script> */}
+function showAddResenaForm() {
+    document.getElementById('add-resena-form').style.display = 'block';
+}
+function hideAddResenaForm() {
+    document.getElementById('add-resena-form').style.display = 'none';
+}
+{/* </script> */}
+
+
+function changeTab(tabId) {
+    // Ocultar todos los contenidos de pestañas
+    document.querySelectorAll('.tab-content').forEach(tab => {
+        tab.style.display = 'none';
+    });
+
+    // Desactivar todas las pestañas
+    document.querySelectorAll('.tabs .tab').forEach(tab => {
+        tab.classList.remove('active');
+    });
+
+    // Mostrar el contenido de la pestaña seleccionada
+    document.getElementById(tabId).style.display = 'block';
+
+    // Activar la pestaña seleccionada
+    let tabs = document.querySelectorAll('.tabs .tab');
+    tabs.forEach(tab => {
+        if (tab.getAttribute('onclick') && tab.getAttribute('onclick').includes(tabId)) {
+            tab.classList.add('active');
+        }
+    });
+}
+
+
 // Inicialización cuando el DOM esté cargado
 document.addEventListener('DOMContentLoaded', function() {
     // Mostrar la sección del dashboard por defecto
